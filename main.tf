dd059e8bd051fa962cb83f57c71a2e5056b538db
kiran shet <54165702+kiranshet@users.noreply.github.com>
2026-06-06 22:52:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,22 @@
 # Create S3 bucket
 
+# Fetch the latest Amazon Linux 2 AMI
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
 # Create EC2 instance
 resource "aws_instance" "demo_ec2" {
-  ami = "ami-0fc5d935ebf8bc3bc"
+  ami           = data.aws_ami.amazon_linux.id
   instance_type = var.ec2_instance_type
   key_name      = "my-key"
-  user_data = file("${path.module}/scripts/install.sh")
+  user_data     = file("${path.module}/scripts/install.sh")
   security_groups = ["default"]
 
   tags = {
@@ -13,3 +24,5 @@ resource "aws_instance" "demo_ec2" {
     Environment = "Dev"
   }
 }
+
+

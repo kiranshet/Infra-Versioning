7454b56332ba96d024994e4dc92253333b82cd21
kiran shet <54165702+kiranshet@users.noreply.github.com>
2026-06-06 23:12:38 +0530

    Change default EC2 instance type to c7i-flex.large

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "ec2_instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t2.medium"
+  default     = "c7i-flex.large"
 }
 
 variable "bucket_name" {

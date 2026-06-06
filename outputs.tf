825bd92c52999fe7d5f94699c22cdac9dae355c0
kiran shet <54165702+kiranshet@users.noreply.github.com>
2026-06-06 22:44:10 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "s3_bucket_name" {
   description = "Name of the created S3 bucket"
-  value       = aws_s3_bucket.demo_bucket.bucket
+ value = var.bucket_name
 }
 
 output "ec2_instance_id" {
